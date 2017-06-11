2e91eecd073f77a000940a0eafad3d3f4e25ada9
Dzaky <dzakyrahmanto@gmail.com>
2017-06-12 06:31:20 +0700

    Change Intro

--- app/src/main/java/com/project/dzakdzak/introwithkotlin/Intro.kt
+++ app/src/main/java/com/project/dzakdzak/introwithkotlin/Intro.kt
@@ -12,10 +12,10 @@ class Intro : AppIntro() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addSlide(AppIntroFragment.newInstance("Hello Food!", "The easiest way to order your favourite food!", R.drawable.cat1, Color.parseColor("#f64c73")))
-        addSlide(AppIntroFragment.newInstance("Great Discounts", "Great Discounts and low rates on every Single service we offer!", R.drawable.cat2, Color.parseColor("#20d2bb")))
-        addSlide(AppIntroFragment.newInstance("Movie Tickets!", "Book the movie tickets for your friends and family!", R.drawable.cat3, Color.parseColor("#3395ff")))
-        addSlide(AppIntroFragment.newInstance("Travel", "Book the tickets of bus, trains and airlines from here!", R.drawable.cat4, Color.parseColor("#c873f4")))
+        addSlide(AppIntroFragment.newInstance("Hello Cat!", "Cat Is Very Cute!", R.drawable.cat1, Color.parseColor("#f64c73")))
+        addSlide(AppIntroFragment.newInstance("Great Cat!", "Great Cat! Yeayyy!", R.drawable.cat2, Color.parseColor("#20d2bb")))
+        addSlide(AppIntroFragment.newInstance("Cat Movie!", "The Movie Is Cool!", R.drawable.cat3, Color.parseColor("#3395ff")))
+        addSlide(AppIntroFragment.newInstance("Traveling Cat", "Yeayyyyyyyy!", R.drawable.cat4, Color.parseColor("#c873f4")))
         // setFadeAnimation();
         setDepthAnimation()
         //        setFadeAnimation();
@@ -26,19 +26,19 @@ class Intro : AppIntro() {
 
     override fun onSkipPressed(currentFragment: Fragment?) {
         super.onSkipPressed(currentFragment)
-        // Do something when users tap on Skip button.
+        // Jika User Klik Skip
         Toast.makeText(this, "You Pressed Skip!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
         super.onDonePressed(currentFragment)
-        // Do something when users tap on Done button.
+        // Jika User Klik Done
         Toast.makeText(this, "You Pressed Done!!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onSlideChanged(oldFragment: Fragment?, newFragment: Fragment?) {
         super.onSlideChanged(oldFragment, newFragment)
-        // Do something when the slide changes.
+        // Jika Slide Berganti
 
     }
 
